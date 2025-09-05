cb0b9c8884b3c93fabfe5d9c0f386dc0b7ec3dee
renewj <renewj@gmail.com>
2025-09-05 17:50:26 -0300

    Update noticias.js

--- pages/api/noticias.js
+++ pages/api/noticias.js
@@ -1,18 +1,18 @@
-import axios from 'axios';
-
 export default async function handler(req, res) {
   try {
-    const response = await axios.get(
+    const response = await fetch(
       'https://www.infomoney.com.br/wp-json/wp/v2/posts?per_page=10'
     );
-    // Formata os dados para o frontend
-    const noticias = response.data.map(post => ({
+    const data = await response.json();
+
+    const noticias = data.map(post => ({
       id: post.id,
       title: post.title.rendered,
       excerpt: post.excerpt.rendered,
       link: post.link,
       date: post.date,
     }));
+
     res.status(200).json(noticias);
   } catch (error) {
     res.status(500).json({ error: 'Não foi possível buscar as notícias.' });
