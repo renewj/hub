8cd3c2a889a35a51ecf9dce97c97312caea13752
renewj <renewj@gmail.com>
2025-09-09 08:51:42 -0300

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+export default function AboutPage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Sobre o Blog</h1>
+        <p>
+          Este é um exemplo de blog feito com Next.js consumindo a API do WordPress.
+        </p>
+      </main>
+      <Footer />
+    </>
+  )
+}
