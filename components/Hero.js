2a3d6e7da61ba8a7de545aaa880d4023c12e22ff
renewj <renewj@gmail.com>
2025-09-09 09:10:05 -0300

    Create Hero.js

---
+++ components/Hero.js
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <section style={{padding: '2rem', background: '#eee', textAlign: 'center'}}>
+      <h1>Bem-vindo ao Blog!</h1>
+      <p>Descubra as últimas novidades e destaques.</p>
+    </section>
+  );
+}
