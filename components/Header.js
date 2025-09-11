0d64fcb3e8e1f2632bab28d195254b679fa41970
renewj <renewj@gmail.com>
2025-09-11 15:29:39 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header style={{ background: "#222", color: "#fff", padding: "1rem" }}>
-      <nav>
+    <header style={{ background: "#000", color: "#fff", padding: "1rem" }}>
+      <nav style={{ background: "#000", color: "#fff", maxWidth: "800px", margin:"0 auto" }}>
         <Link href="/"><a style={{ color: "#fff", marginRight: "2rem", fontWeight: "bold" }}>Home</a></Link>
         <Link href="/categories"><a style={{ color: "#fff", marginRight: "2rem" }}>Categorias</a></Link>
         <Link href="/about"><a style={{ color: "#fff" }}>Sobre</a></Link>
