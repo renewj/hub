3d13b4ccd441bff81b0a4e302f582931b1009215
renewj <renewj@gmail.com>
2025-09-11 16:01:53 -0300

    Delete components directory

--- components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer style={{ background: "#222", color: "#fff", padding: "1rem", textAlign: "center" }}>
-      <small>&copy; {new Date().getFullYear()} - Meu Blog Next.js & WordPress</small>
-    </footer>
-  );
-}
--- components/Hero.js
+++
@@ -1,8 +0,0 @@
-export default function Hero() {
-  return (
-    <section style={{padding: '2rem', background: '#eee', textAlign: 'center'}}>
-      <p>Olá! Quer que eu te ajude a encontrar conteúdos alinhados ao seu perfil?</p>
-      <p>SIM | NÃO</p>
-    </section>
-  );
-}
