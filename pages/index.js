bddd36745a82d90f70461f0ef3a2899a71be2216
renewj <renewj@gmail.com>
2025-09-11 15:34:33 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,6 @@ export default function Home() {
       <Script src="/assets/js/main.js" strategy="afterInteractive" />
         <Hero />
     <main style={{margin:'0 auto',maxWidth:'800px'}}>
-        <div><form><label>Como podemos ajudar você hoje?</label><input type="text" name="prompt" value="" placeholder="Enviar sua mensagem para nós" /></form></div>
         <FeaturedPosts posts={featured} />
         <Categories categories={categories} />
         <PostList posts={posts} />
